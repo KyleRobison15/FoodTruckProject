8f436f9339f3d3f3ff246f762c070d33c67cb574
Kyle Robison <kylerobison15@gmail.com>
2021-05-22 08:13:20 -0600

    built out FoodTruck class and some basic code for the app

--- src/com/skilldistillery/app/FoodTruck.java
+++ src/com/skilldistillery/app/FoodTruck.java
@@ -2,4 +2,43 @@ package com.skilldistillery.app;
 
 public class FoodTruck {
 
+	private static int nextTruckId;
+	private int truckId;
+	private String name;
+	private String foodType;
+	private int rating;
+
+	public FoodTruck() {
+		this.name = null;
+		this.foodType = null;
+		this.rating = 0;
+		this.truckId = nextTruckId;
+		nextTruckId++;
+
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFoodType() {
+		return foodType;
+	}
+
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
 }
--- src/com/skilldistillery/app/FoodTruckApp.java
+++ src/com/skilldistillery/app/FoodTruckApp.java
@@ -1,10 +1,62 @@
 package com.skilldistillery.app;
 
+import java.util.Scanner;
+
 public class FoodTruckApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		String name;
+		String foodType;
+		int rating;
+		FoodTruck[] userTrucks;
 
+		System.out.println("Please enter up to five food trucks.");
+		System.out.println("If you are done entering trucks, please enter 'quit' for the next name.");
+		
+		//TODO store the returned array of food trucks in a variable of type FoodTruck []
+//		userTrucks = collectUserFoodTrucks();
+		
+		//TODO create menu method that displays menu options (user story #2)
+
+	}
+
+	public FoodTruck[] collectUserFoodTrucks(Scanner input, String name, String foodType, int rating) {
+		//This method will create new instances of FoodTruck objects 
+		//It will then assign the FoodTruck's instance fields by taking in user input for each field
+		//It will then store each FoodTruck in a FoodTruck array
+		//It will then return an array of these FoodTruck objects 
+		 
+			//TODO Add loop here to run the array collector until user enters name of 'quit'
+			//TODO Make sure the array has the same number of indexes as the number of trucks they enter
+			
+		
+			FoodTruck [] userTrucks = new FoodTruck[0]; //initialized this array with 0 indexes 
+														//so that the loop below can determine number of indexes 
+														//of array to be returned.
+		
+			//Instantiate a new FoodTruck object (and set unique ID automatically)
+			FoodTruck foodTruck = new FoodTruck();
+			
+			//Set name field
+			System.out.print("What is the name of this food truck?");
+			name = input.nextLine();
+			foodTruck.setName(name);
+			System.out.println();
+			
+			//Set foodType field
+			System.out.print("What type of deliciousness does this food truck sell?");
+			foodType = input.nextLine();
+			foodTruck.setFoodType(foodType);
+			System.out.println();
+			
+			//Set rating field
+			System.out.print("What overall rating (out of 10) would you give this food truck?");
+			rating = input.nextInt();
+			foodTruck.setRating(rating);
+			System.out.println();
+		
+		return userTrucks;
 	}
 
 }
